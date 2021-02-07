643d58363ea23a30d1f8ef7c35bb73127a67e634
Mario Sanchez <mgsanchezdev@gmail.com>
2021-02-06 23:20:58 -0300

    Pograme la conexion a la base de datos y comprobe la correcta conexion

--- src/java/com/soap/config/Conexion.java
+++ src/java/com/soap/config/Conexion.java
@@ -6,10 +6,33 @@
 
 package com.soap.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Marito
  */
 public class Conexion {
+    Connection con = null;
     
+    public Connection conectar() throws ClassNotFoundException, SQLException{
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbbilletera","root","");
+        }catch(ClassNotFoundException ex){
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE,null,ex);
+        }
+        return con;
+    }
+    
+    public static void main(String[] args) throws ClassNotFoundException, SQLException{
+    Conexion con = new Conexion();
+    System.out.println("Me conecte a la base de datos" + con.conectar());    
 }
+}
+
